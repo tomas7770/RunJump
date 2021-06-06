ea9e5467cb5736e3a544ce8999247c8f18670cbb
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2021-06-06 15:15:16 +0000

    Make the difficulty switch button functional

--- scenes/TitleHUD.gd
+++ scenes/TitleHUD.gd
@@ -1,13 +1,24 @@
 extends CanvasLayer
 
 const SETTINGS_HUD = preload("SettingsHUD.tscn")
+const DIFFICULTY = GlobalVariables.DIFFICULTY
+const DIFFICULTY_STRINGS = {DIFFICULTY.NORMAL:"Normal", DIFFICULTY.EASY:"Easy"}
 
 func _ready():
-	update_hscore(GlobalVariables.highscore_get())
+	_update_hscore()
+	_update_difficulty_label()
 	$VersionLabel.text = GlobalVariables.GAME_VERSION
 
-func update_hscore(score):
-	$HighScore.text = str("High Score: ",score)
+func _update_hscore():
+	$HighScore.text = str("High Score: ", GlobalVariables.highscore_get())
+
+func _update_difficulty_label():
+	$DifficultyButton.text = DIFFICULTY_STRINGS[GlobalVariables.selected_difficulty]
 
 func _on_SettingsButton_pressed():
 	get_parent().add_child(SETTINGS_HUD.instance())
+
+func _on_DifficultyButton_pressed():
+	GlobalVariables.selected_difficulty = (GlobalVariables.selected_difficulty+1)%(DIFFICULTY.size())
+	_update_hscore()
+	_update_difficulty_label()
