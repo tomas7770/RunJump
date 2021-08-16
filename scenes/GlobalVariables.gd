b81d58a3fe9f4c76b51c0df7434047b4587e1deb
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2021-08-16 13:15:22 +0000

    Generalize TitleHUD auto resize function

--- scenes/GlobalVariables.gd
+++ scenes/GlobalVariables.gd
@@ -77,5 +77,18 @@ func load_data():
 					self[key] = saved_data
 	save_game.close()
 
+func resize_control_toSafeArea(control):
+	# Resizes a fullscreen Control to fit window safe area
+	# I actually have no idea how this works
+	# Thanks to kleonc (https://github.com/godotengine/godot/issues/49887)
+	var window_to_root = Transform2D.IDENTITY.scaled(get_tree().root.size / OS.window_size)
+	var safe_area_root = window_to_root.xform(OS.get_window_safe_area())
+	assert(control.get_viewport() == get_tree().root, "Assumption: control is not in a nested Viewport")
+	var parent_to_root = control.get_viewport_transform() * control.get_global_transform() * control.get_transform().affine_inverse()
+	var root_to_parent = parent_to_root.affine_inverse()
+	var safe_area_relative_to_parent = root_to_parent.xform(safe_area_root)
+	control.rect_position = safe_area_relative_to_parent.position
+	control.rect_size = safe_area_relative_to_parent.size
+
 func _exit_tree():
 	save_data()
