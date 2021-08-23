434d3dd7a3754eeffdbeedc829a44fe1104d0b85
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2021-08-23 21:58:36 +0000

    Increase Game Over popup's high score fade delay

--- scenes/HUD/GameOver.gd
+++ scenes/HUD/GameOver.gd
@@ -5,13 +5,13 @@ var showing_new_highscore = false
 func on_new_highscore(old, new):
 	$HighScoreLabel.text = "New High Score!"
 	$NewScoreLabel.text = "("+str(old)+" -> "+str(new)+")"
-	$InfoFadeTimer.start(1)
+	$InfoFadeTimer.start(2)
 
 func _on_InfoFadeTimer_timeout():
 	if showing_new_highscore:
 		$AnimationPlayer.play_backwards("GameOverInfoFade")
-		$InfoFadeTimer.start(1)
+		$InfoFadeTimer.start(2)
 	else:
 		$AnimationPlayer.play("GameOverInfoFade")
-		$InfoFadeTimer.start(2)
+		$InfoFadeTimer.start(3)
 	showing_new_highscore = !showing_new_highscore
