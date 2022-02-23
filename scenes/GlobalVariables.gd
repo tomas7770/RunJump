453cdc10af478525890d9fccf86904add52da0e2
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2022-02-23 19:00:22 -0100

    Update game version

--- scenes/GlobalVariables.gd
+++ scenes/GlobalVariables.gd
@@ -2,7 +2,7 @@ extends Node
 
 signal on_pause
 
-const GAME_VERSION = "Dev Version (v0.3.0+)"
+const GAME_VERSION = "v0.4.0 Beta"
 enum DIFFICULTY {NORMAL, EASY, FAST}
 const DIFFICULTY_STRINGS = {
 	DIFFICULTY.NORMAL:"Normal",
