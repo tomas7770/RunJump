f3152332cc07a81b6e66f71ef5a964747bb2a080
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2022-02-25 22:47:29 -0100

    GameOver: Add manual toggle for New High Score label
    Toggle between the New High Score message and the score comparison

--- scenes/HUD/GameOver.gd
+++ scenes/HUD/GameOver.gd
@@ -7,12 +7,19 @@ func on_new_highscore(old, new):
 	$NewScoreLabel.text = "("+str(old)+" -> "+str(new)+")"
 	$HighScoreLabel.modulate = Color(0xffbf00ff)
 	$InfoFadeTimer.start(2)
+	$LabelSwitchButton.visible = true
 
-func _on_InfoFadeTimer_timeout():
+func _on_InfoFadeTimer_timeout(do_restart = true):
 	if showing_new_highscore:
 		$AnimationPlayer.play_backwards("GameOverInfoFade")
-		$InfoFadeTimer.start(2)
+		if do_restart:
+			$InfoFadeTimer.start(2)
 	else:
 		$AnimationPlayer.play("GameOverInfoFade")
-		$InfoFadeTimer.start(3)
+		if do_restart:
+			$InfoFadeTimer.start(3)
 	showing_new_highscore = !showing_new_highscore
+
+func _on_LabelSwitchButton_pressed():
+	$InfoFadeTimer.stop()
+	_on_InfoFadeTimer_timeout(false)
