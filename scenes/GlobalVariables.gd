8eed8174668cc530705b3959a3bf98ed40d8cf3b
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2022-03-01 12:44:28 -0100

    Update version and export presets

--- scenes/GlobalVariables.gd
+++ scenes/GlobalVariables.gd
@@ -2,7 +2,7 @@ extends Node
 
 signal on_pause
 
-const GAME_VERSION = "v0.4.0 Beta"
+const GAME_VERSION = "v0.4.0"
 enum DIFFICULTY {NORMAL, EASY, FAST}
 const DIFFICULTY_STRINGS = {
 	DIFFICULTY.NORMAL:"Normal",
