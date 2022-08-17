6286b9b64621b61a029044aff9b93ad7d47a7218
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2022-08-17 20:01:41 +0000

    Add another music

--- scenes/MusicPlayer.gd
+++ scenes/MusicPlayer.gd
@@ -1,15 +1,29 @@
 extends Node
 
+const track_list = [
+	"res://sounds/music/fasttheme.mp3",
+	"res://sounds/music/upbeatoverworld.mp3"
+]
+
+var current_track = 0
 var music
 
 func _ready():
 	pause_mode = PAUSE_MODE_PROCESS
 	music = AudioStreamPlayer.new()
 	add_child(music)
-	music.stream = load("res://sounds/music/fasttheme.mp3")
+	music.stream = load(track_list[current_track])
+	music.connect("finished", self, "_on_finished")
 
 func toggle_music(enabled):
 	if enabled:
 		music.play()
 	else:
 		music.stop()
+
+func _on_finished():
+	current_track += 1
+	if current_track > track_list.size()-1:
+		current_track = 0
+	music.stream = load(track_list[current_track])
+	music.play()
